a8629d20028b9929959f4f8c8c7ce368699f9622
Mohammad <mghmgh1281375@gmail.com>
2021-02-12 15:01:23 +0330

    Use argparser instead of pure sys.argv

--- data/preproc.py
+++ data/preproc.py
@@ -1,9 +1,13 @@
-
+import argparse
 from pathlib import Path
 
 get_value = lambda a: a.split('\t')[1]
 
-true_dir = Path('/home/mohammad/Projects/SRU_PHN/data/true/')
+parser = argparse.ArgumentParser()
+parser.add_argument('--true_dir', help='true folder path', required=True, type=Path)
+args = parser.parse_args()
+
+true_dir = Path(args.true_dir)
 with open('data/gt/words.txt', 'w') as _file:
     for _name in true_dir.iterdir():
         content = open(_name, 'r').read()
