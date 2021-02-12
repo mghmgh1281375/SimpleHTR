4e5f101b65a27260ed21a1c2634be46e5f21c3d9
Mohammad <mghmgh1281375@gmail.com>
2021-02-12 14:43:23 +0330

    Fix wrong path

--- data/preproc.py
+++ data/preproc.py
@@ -4,7 +4,7 @@ from pathlib import Path
 get_value = lambda a: a.split('\t')[1]
 
 true_dir = Path('/home/mohammad/Projects/SRU_PHN/data/true/')
-with open('data/dataset/gt/words.txt', 'w') as _file:
+with open('data/gt/words.txt', 'w') as _file:
     for _name in true_dir.iterdir():
         content = open(_name, 'r').read()
         lines = content.split('\n')
